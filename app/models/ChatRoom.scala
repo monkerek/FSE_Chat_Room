a4b934f7d7946305b89a934b839bead64d3922e6
monkerek <monkerek@hotmail.com>
2015-01-04 15:49:08 +0800

    delete robot

--- app/models/ChatRoom.scala
+++ app/models/ChatRoom.scala
@@ -1,54 +1,21 @@
 package models
 
-import akka.actor._
-import akka.util.duration._
-
 import play.api._
 import play.api.libs.json._
 import play.api.libs.iteratee._
 import play.api.libs.concurrent._
-
+import play.api.Play.current
+import akka.actor._
+import akka.util.duration._
 import akka.util.Timeout
 import akka.pattern.ask
-
-import play.api.Play.current
-
-object Robot {
-  
-  def apply(chatRoom: ActorRef) {
-    
-    // Create an Iteratee that log all messages to the console.
-    val loggerIteratee = Iteratee.foreach[JsValue](event => Logger("robot").info(event.toString))
-    
-    implicit val timeout = Timeout(1 second)
-    // Make the robot join the room
-    chatRoom ? (Join("Robot")) map {
-      case Connected(robotChannel) => 
-        // Apply this Enumerator on the logger.
-        robotChannel |>> loggerIteratee
-    }
-    
-    // Make the robot talk every 30 seconds
-    Akka.system.scheduler.schedule(
-      30 seconds,
-      30 seconds,
-      chatRoom,
-      Talk("Robot", "I'm still alive")
-    )
-  }
-  
-}
 
 object ChatRoom {
   
   implicit val timeout = Timeout(1 second)
   
   lazy val default = {
-    val roomActor = Akka.system.actorOf(Props[ChatRoom])
-    
-    // Create a bot user (just for fun)
-    Robot(roomActor)
-    
+    val roomActor = Akka.system.actorOf(Props[ChatRoom])    
     roomActor
   }
 
